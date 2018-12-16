cdfa49fe1270d490e0dd0b854e269f432a1d0b55
brian-regan <brianmjregan@gmail.com>
2018-12-16 17:50:55 +0100

    first push

---
+++ app/js/main.js
@@ -0,0 +1,66 @@
+require([
+    "esri/views/MapView",
+    "esri/widgets/Legend",
+    "esri/WebMap"
+  ],
+  function(
+    MapView, Legend, WebMap
+  ) {
+
+    var webmap = new WebMap({
+      portalItem: { // autocasts as new PortalItem()
+        id: "01247f5011ca4515aadcf2a5d471a7e0"
+      }
+    });
+
+    var view = new MapView({
+      container: "viewDiv",
+      map: webmap
+    });
+
+    var p25_layer_toggle = document.getElementById("toggle");
+
+    
+
+    view.when(function() {
+      // get the first layer in the collection of operational layers in the WebMap
+      // when the resources in the MapView have loaded.
+      var p10 = webmap.layers.getItemAt(0);
+      var p25 = webmap.layers.getItemAt(1);
+      p10.id = "p10_layer"
+      p25.id = "p25_layer"
+
+      var legend = new Legend({
+        view: view,
+        layerInfos: [
+        {
+          layer: p10
+        },
+        {
+          layer: p25
+        }
+        ]
+      });
+
+      // Add widget to the bottom right corner of the view
+      view.ui.add(legend, "bottom-right");
+
+      /*****************************************************************
+     * The visible property on the layer can be used to toggle the
+     * layer's visibility in the view. When the visibility is turned off
+     * the layer is still part of the map, which means you can access
+     * its properties and perform analysis even though it isn't visible.
+     *******************************************************************/
+
+    p25_layer_toggle.addEventListener("change", function () {
+      p25.visible = toggle.checked;
+      p10.visible = ! toggle.checked
+    });
+
+    });
+
+    
+
+  });
+
+  
---
+++ app/js/scrolling.js
@@ -0,0 +1,8 @@
+// Based on https://codepen.io/nxworld/pen/OyRrGy
+
+$(function() {
+    $('a[href*="#"]').on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 250, 'linear');
+    });
+  });
